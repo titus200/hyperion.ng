9c21c719be5a4f255300f6cf1e83aa9471237d20
brindosch <edeltraud70@gmx.de>
2019-09-21 12:18:53 +0200

    try thread fix

--- libsrc/hyperion/Hyperion.cpp
+++ libsrc/hyperion/Hyperion.cpp
@@ -62,7 +62,7 @@ Hyperion::Hyperion(const quint8& instance)
 
 Hyperion::~Hyperion()
 {
-	freeObjects(false);
+	
 }
 
 void Hyperion::start()
@@ -150,6 +150,7 @@ void Hyperion::start()
 
 void Hyperion::stop()
 {
+	freeObjects(false)
 	emit finished();
 	thread()->wait();
 }
